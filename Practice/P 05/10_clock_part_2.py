96645609b9b69889d3182e9efad95c719ceefcea
TSVTLN <tsvtln@abv.bg>
2023-05-22 18:07:16 +0300

    Update 10_clock_part_2.py

---
+++ Practice/P 05/10_clock_part_2.py
@@ -0,0 +1,7 @@
+# import time
+for h in range(0, 24):
+    for m in range(0, 60):
+        for s in range(0, 60):
+            print(f'{h} : {m} : {s}')
+        # print(f'{h}:{m:02d}')
+        # time.sleep(1)
